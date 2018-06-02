593bd8d37fce54e9b77a256f2c086a288030347f
Bodomate <bodo.mate@gmail.com>
2018-06-02 12:24:37 +0200

    First commit.

---
+++ src/main/java/com/elsoproject/controller/HomeController.java
@@ -0,0 +1,61 @@
+package com.elsoproject.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.elsoproject.domain.Story;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String stories(Model model, Locale locale) {
+		model.addAttribute("pageTitle","Minden napra egy SFJ sztori");
+		model.addAttribute("stories",getStories());
+		System.out.println((String.format("Request received. Language: %s, Country: %s %n",locale.getLanguage(), locale.getDisplayCountry())));
+		return "stories";
+	}
+	
+	@RequestMapping("/user/{id}")
+	public String searchForUser(@PathVariable(value="id") String id) throws Exception {
+		if (id == null)
+			throw new Exception("Nincs ilyen id-val felhasználónk!");
+		return "user";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(HttpServletRequest rA, Exception ex, Model model) {
+		model.addAttribute("errMessage", ex.getMessage());
+		return "exceptionHandler";
+	}
+	
+	private ArrayList<Story> getStories() {
+		ArrayList<Story> stories = new ArrayList<>();
+		
+//		Story story1 = new Story();
+//		story1.setTitle("Első sztorim");
+//		story1.setPosted(new Date());
+//		story1.setAuthor("Bogyi");
+//		story1.setContent("<p>Na ez már éles adat!</p>");
+//
+//		Story story2 = new Story();
+//		story2.setTitle("Második sztorim");
+//		story2.setPosted(new Date());
+//		story2.setAuthor("Feri");
+//		story2.setContent("<p>Feri nem érdekel</p>");
+//
+//		stories.add(story1);
+//		stories.add(story2);
+		
+		return stories;
+	}
+}
---
+++ src/main/java/com/elsoproject/domain/Story.java
@@ -0,0 +1,73 @@
+package com.elsoproject.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Story {
+
+	@GeneratedValue
+	@Id
+	private Long id;
+	private String title;
+	private String content;
+	private Date posted;
+	@ManyToOne
+	private Blogger blogger;
+	
+	private Story() {}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public Date getPosted() {
+		return posted;
+	}
+
+	public void setPosted(Date posted) {
+		this.posted = posted;
+	}
+
+	public Blogger getBlogger() {
+		return blogger;
+	}
+
+	public void setBlogger(Blogger author) {
+		this.blogger = author;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Story [title=" + title + "]";
+	}
+	
+	
+}
